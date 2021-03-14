7337bc7bbad08a0c261cf71316effa1a260fb659
Paula Diniz <bomdiapaulaa@gmail.com>
2021-03-13 22:34:03 -0300

    Adding base components: poxtbox and header.

---
+++ components/PostBox.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="post-box">
+		<div class="post-title">
+			title
+		</div>
+		<div class="post-content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Convallis convallis tellus id interdum velit. Ipsum consequat nisl vel pretium lectus. Augue ut lectus arcu bibendum
+				at varius vel pharetra vel. Morbi quis commodo odio aenean sed adipiscing diam donec. Quam lacus suspendisse faucibus interdum.
+				Sodales neque sodales ut etiam sit amet nisl. In nisl nisi scelerisque eu. Felis eget nunc lobortis mattis aliquam faucibus.
+				Sed viverra ipsum nunc aliquet. Enim ut sem viverra aliquet. Ac turpis egestas sed tempus urna et pharetra pharetra.
+				Congue eu consequat ac felis.</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.post-title {
+	display: flex;
+	align-items: center;
+	height: 50px;
+	background-color: #CDDCD9;
+	color: #256974;
+	font-size: 20px;
+	text-transform: uppercase;
+	font-weight: 600;
+	padding: 0 20px;
+}
+.post-box {
+	width: 800px;
+	min-height: 350px;
+}
+.post-content {
+	text-align: justify;
+	color: #4E4E4E;
+}
+p {
+	margin-top: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="header">
+      header
     </div>
+    <section class="content">
+      <post-box/>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostBox from '../components/PostBox.vue'
 
 export default {
   components: {
-    Logo
+    PostBox
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: #dc8d80;
+  color: #F7F3F0;
+}
+.content {
   display: flex;
   justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
